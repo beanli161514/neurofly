b39ecabc00e772acd002d9d9379572d6d07557f0
RyuuYou <ryuuyou0529@gmail.com>
2025-07-21 21:31:59 +0800

    fix bug of loading pth files

--- neurofly/model/__init__.py
+++ neurofly/model/__init__.py
@@ -1,11 +1,12 @@
-import os
 import torch
-
+from pathlib import Path
+ 
 # defalut weight paths
-package_dir = os.path.dirname(os.path.abspath(__file__))
-default_seger_weight_path = os.path.join(package_dir, 'seg_fiber/universal_tiny.pth')
-default_dec_weight_path = os.path.join(package_dir, 'resin/mpcn_dumpy.pth')
-default_transformer_weight_path = os.path.join(package_dir, 'tracer/next_pos.pth')
+package_dir = Path(__file__).resolve().parent
+
+default_seger_weight_path = str(package_dir / 'seg_fiber' / 'universal_tiny.pth')
+default_dec_weight_path = str(package_dir / 'resin' / 'mpcn_dumpy.pth')
+default_transformer_weight_path = str(package_dir / 'tracer' / 'next_pos.pth')
 
 # if nvidia gpu is available, use pytorch to inference, else use tinygrad
 if torch.cuda.is_available():
